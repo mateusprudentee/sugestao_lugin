9b6fc3f23363907e606e6478a66ede6bdbfa90f3
Mateus Horbach <mateus.horbach@unimed279.com.br>
2025-06-02 09:31:13 -0400

    Primeiro commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+body {
+  background: black;
+  color: white; /* Adiciona uma cor de texto clara para contraste */
+  margin: 0; /* Remove margens padrão do body */
+  font-family: Arial, sans-serif; /* Define uma fonte padrão */
+}
+
+h1 {
+  margin: 40px 0; /* Altera a margem do h1 */
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983; /* Cor dos links */
+}
+
+a:hover {
+  color: #ff9800; /* Cor dos links ao passar o mouse */
+}
+</style>
